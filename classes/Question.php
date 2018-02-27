05b505ab28f58c7de282daa539277489a592db82
Tyler Bezera <tbezera2@mail.greenriver.edu>
2018-02-27 15:01:38 -0500

    Added supporting methods to Question class

--- classes/Question.php
+++ classes/Question.php
@@ -23,4 +23,50 @@ class Question
         $this->_category = $category;
         $this->_correctIndex = $correctIndex;
     }
+
+    /**
+     * Getter for question
+     * @return String Main question
+     */
+    public function getQuestion()
+    {
+        return $this->_question;
+    }
+
+
+    /**
+     * Getter for the possible answers array
+     * @return Array<String> an array of the questions
+     */
+    public function getPossibleAnswers()
+    {
+        return $this->_possibleAnswers;
+    }
+
+    /**
+     * Getter for the catergory id
+     * @return String catergory this question belongs to
+     */
+    public function getCategory()
+    {
+        return $this->_category;
+    }
+
+    /**
+     * Getter for the correct answer index, used for getting the answer from the answers array
+     * @return int index of correct answer
+     */
+    public function getCorrectIndex()
+    {
+        return $this->_correctIndex;
+    }
+
+    /**
+     * Getter for the correct answer, used to compare against user answer
+     * @return String Correct answer from array
+     */
+    public function getCorrectAnswer()
+    {
+        return $this->_possibleAnswers[$this->_correctIndex - 1];
+    }
 }
