5430ca2646253b4516de2605398f6b54851828bd
Tyler Bezera <tbezera2@mail.greenriver.edu>
2018-02-28 17:10:29 -0500

    Functionality for pulling from api, parsing json, and displaying on play page

--- models/api-functions.php
+++ models/api-functions.php
@@ -3,15 +3,23 @@
 /**
  *
  */
-function getQuestions()
+if(isset($_POST['action']))
+{
+    if ($_POST['action'] == 'question')
+    {
+        getQuestion();
+    }
+}
+
+function getQuestion()
 {
     $client = new GuzzleHttp\Client();
-    $res = $client->request('GET', 'https://qriusity.com/v1/questions');
+    $res = $client->request('GET', 'https://qriusity.com/v1/questions?page=2&limit=1');
 
-    echo $res->getStatusCode();
-
-    echo $res->getHeader('content-type');
-
+    //
+//    echo $res->getStatusCode();
+//
+//    echo $res->getHeader('content-type');
     echo $res->getBody();
 }
 
