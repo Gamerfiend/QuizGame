13810a7c5f6b4af97d123806827a9b3e4bacbf7d
Tyler Bezera <tbezera2@mail.greenriver.edu>
2018-03-07 15:47:57 -0500

    Seperated post ajax controller

---
+++ controller/question.js
@@ -0,0 +1,17 @@
+
+$.post('328/QuizGame/models/api-functions.php', {action: 'question'}, function( data ) {
+
+    var index = data.indexOf(']');
+    data = data.substring(1,index);
+    console.log(data);
+    var obj = JSON.parse(data);
+    console.log(obj);
+    $('#answer1').val(obj.option1);
+    $('#answer2').val(obj.option2);
+    $('#answer3').val(obj.option3);
+    $('#answer4').val(obj.option4);
+    $('#question').html(obj.question);
+    $('#category').html(obj.category.name);
+
+    <!-- Route to same page, if answer is correct new question else end game -->
+});
